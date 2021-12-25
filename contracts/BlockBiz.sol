0b009d8755b873b732dc8b066675b31925a086a9
Elim Poon <elimviolinist@gmail.com>
2021-12-25 15:11:50 -0800

    Installed Waffle & Chai

--- contracts/BlockBiz.sol
+++ contracts/BlockBiz.sol
@@ -1,2 +1,23 @@
+/// @title Contract that handles equity ownership
+/// @author Elim Poon
+/// @notice My personal project
+
 pragma solidity ^0.7.0;
 
+import "hardhat/console.sol";
+
+contract BlockBiz {    
+    /// @dev Still in development
+
+    string public companyName;
+    mapping(address => uint) sharesHeldBy; 
+
+    constructor(string memory _companyName, uint _initShares) payable {
+        companyName = _companyName;
+        sharesHeldBy[msg.sender] = _initShares;
+    }
+
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,3 @@
-require("@nomiclabs/hardhat-waffle");
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
