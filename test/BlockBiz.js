704195276e98154210249e4741f19b0b17586610
Elim Poon <elimviolinist@gmail.com>
2021-12-25 17:10:57 -0800

    Attempted testing

--- test/BlockBiz.js
+++ test/BlockBiz.js
@@ -1,13 +1,15 @@
 const { expect } = require("chai");
 
-describe("Token contract", function () {
-  it("Deployment should assign the total supply of tokens to the owner", async function () {
+describe("BlockBiz contract", function () {
+  it("should initialize with all shares held by sender.", async function () {
     const [owner, addr1, addr2] = await ethers.getSigners();
 
     const BlockBiz = await ethers.getContractFactory("BlockBiz");
 
-    const hardhatBlockBiz = await BlockBiz.deploy();
+    const shares = Number(100);
 
-    const ownerBalance = await hardhatBlockBiz.getBalance(owner.address);
+    const hardhatBlockBiz = await BlockBiz.deploy("Hello", shares);
+
+    expect(await hardhatBlockBiz.getBalance()).to.equal(shares);
   });
 });
