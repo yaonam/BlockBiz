1d5af6aa53650b77d58f9ae725a1077311e61eca
Elim Poon <elimviolinist@gmail.com>
2021-12-26 17:27:59 -0800

    Converted motion contract to handle multiple

--- contracts/motion.sol
+++ contracts/motion.sol
@@ -11,23 +11,43 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Motion{
     // string private description;
     enum MotionState{ongoing, passed, rejected}
-    struct Vote {
-        address token;
-        uint yay;
-        uint nay;
+    struct MotionInfo {
+        string name;
+        ERC20 votingToken;
+        uint yays;
+        uint nays;
         MotionState state;
     }
-    mapping (string => Vote) motions;
 
-    event MotionProposed(string indexed _description);
-    event VotesCounted(string indexed _description, uint _yay, uint _nay);
-    event MotionPassed(string indexed _description, bool _motionPassed);
+    mapping (bytes32 => MotionInfo) private motions;
 
-    function proposeMotion(string memory _description, address _token) external {
-        MotionState state = MotionState.ongoing;
-        Vote memory vote = Vote(_token, 0, 0, state);
-        motions[_description] = vote;
+    event MotionProposed(string indexed _name, bytes32 indexed key);
 
-        emit MotionProposed(_description);
+    function proposeMotion(string memory _name, address _token) external returns (bytes32){
+        bytes32 key = keccak256(abi.encodePacked(block.timestamp, _name, msg.sender)); // Create key for motion identification
+        motions[key] = MotionInfo(_name, ERC20(_token), 0, 0, MotionState.ongoing); // Instatiate motion
+
+        emit MotionProposed(_name, key);
+        return key;
+    }
+
+    // function getMotionInfo(uint _key) external view returns (MotionInfo memory){
+    //     return motions[_key];
+    // }
+
+    function getYays(bytes32 _key) external view returns (uint) {
+        return motions[_key].yays;
+    }
+
+    // function getNays(uint _key) external view returns (uint) {
+    //     return motions[_key].nays;
+    // }
+
+    function vote(bytes32 _key, bool _for) external payable {
+        if (_for) { // Vote yay
+            motions[_key].yays += 1;
+        } else { // Vote nay
+            motions[_key].nays += 1;
+        }
     }
 }
