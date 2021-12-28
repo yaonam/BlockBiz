94c95cf8be3bdc0d537d17e621a8611f38613892
Elim Poon <elimviolinist@gmail.com>
2021-12-27 16:46:27 -0800

    Cleaned up contracts

--- contracts/BlockBiz.sol
+++ contracts/BlockBiz.sol
@@ -7,8 +7,6 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "./motion.sol";
-import "hardhat/console.sol";
 
 contract BlockBiz is ERC20{
     /// @dev Still in development
